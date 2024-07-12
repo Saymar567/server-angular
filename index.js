e13b023afe9029f1f6ed16ec46ffa36a223127ed
Mikel Jimenez <majc1907@gmail.com>
2024-07-12 12:16:40 +0200

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const connectDB = require('./config/db')
 const cors = require("cors")
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
 connectDB()
 app.use(cors())
