140718e4defc421d621a645887d58f0916610557
Mikel Jimenez <majc1907@gmail.com>
2024-07-12 12:27:20 +0200

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const connectDB = require('./config/db')
 const cors = require("cors")
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 connectDB()
 app.use(cors())
@@ -12,9 +12,6 @@ app.use(express.json())
 
 app.use('/api/productos', require('./routes/product'));
 
-app.get('/', (req, res) => {
-    res.send('Hola, Mundo')
-})
 
 app.listen(PORT, () => {
 console.log(`Successfully connected to port ${PORT}`)
